6efa642ac9bd6a4f184f0c1de1c0ac0ad276ca8e
rmlzy <liuzhiyuan2018@gmail.com>
2019-01-23 15:20:13 +0800

    集成 react-router-dom

--- app/App.js
+++ app/App.js
@@ -1,20 +1,33 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import {
-    Button
-} from '@alifd/next';
-import avatar from '../public/avatar.jpeg';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './styles/global.scss';
+import HomePage from './pages/home';
+import LoginPage from './pages/login';
+import AboutPage from './pages/about';
 
 class App extends Component {
     render() {
         return (
-            <div>
-                <img src={avatar} width={100} />
-                <p>
-                    <Button type='primary'>Hello React!</Button>
-                </p>
-            </div>
+            <Router>
+                <React.Fragment>
+                    <ul>
+                        <li>
+                            <Link to='/'>Home</Link>
+                        </li>
+                        <li>
+                            <Link to='/login'>Login</Link>
+                        </li>
+                        <li>
+                            <Link to='/about'>About</Link>
+                        </li>
+                    </ul>
+
+                    <Route exact path='/' component={HomePage} />
+                    <Route path='/about' component={AboutPage} />
+                    <Route path='/login' component={LoginPage} />
+                </React.Fragment>
+            </Router>
         );
     }
 }
--- app/pages/home/index.js
+++ app/pages/home/index.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import avatar from '../../../public/avatar.jpeg';
+import {Button} from '@alifd/next';
 
 class Home extends Component {
     static propTypes = {};
@@ -7,7 +9,10 @@ class Home extends Component {
     render() {
         return (
             <div>
-                Home
+                <img src={avatar} width={100}/>
+                <p>
+                    <Button type='primary'>Hello React!</Button>
+                </p>
             </div>
         );
     }
